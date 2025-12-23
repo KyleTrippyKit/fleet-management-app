13a5ac01f90c513eaf33c95fc7922631d650520d
Kyle Trippy Kit <rigsby.kyle@yahoo.com>
2025-12-23 10:10:47 -0400

    Your message

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# config/importmap.rb
+
 # Pin application entry point
 pin "application", preload: true
 
@@ -6,11 +8,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
-# Chart.js and date adapter
-pin "chart.js", to: "chart.js" # @4.5.1
-pin "chartjs-adapter-date-fns", to: "chartjs-adapter-date-fns.js" # @3.0.0
-pin "date-fns", to: "date-fns.js" # @4.1.0
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+# Remove Chart.js from importmap - we'll load it manually only when needed
+# pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.js"
+# pin "@kurkle/color", to: "https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.2/dist/color.esm.js"
 
 # Pin all controllers from app/javascript/controllers
 pin_all_from "app/javascript/controllers", under: "controllers"
